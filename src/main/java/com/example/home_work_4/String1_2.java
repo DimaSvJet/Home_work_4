cf9319e1fa089c2dd4be6dc5030560c910d85fbf
DimaSv <dimasvjet@gmail.com>
2023-02-26 15:59:40 +0200

    String1-2

--- src/main/java/com/example/home_work_4/String1_2.java
+++ src/main/java/com/example/home_work_4/String1_2.java
@@ -2,15 +2,14 @@ package com.example.home_work_4;
 
 import java.util.Scanner;
 
-public class String1_1 {
+public class String1_2 {
 
     public static void main(String[] args) {
 
-        String s1 = "<<>>";
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter your word: ");
         String yourWord = scanner.nextLine();
-        System.out.println(s1.substring(0,2)+yourWord+s1.substring(2));
+        System.out.println(yourWord.substring(0,yourWord.length()/2));
 
 
 
