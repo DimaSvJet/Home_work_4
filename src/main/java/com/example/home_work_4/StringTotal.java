d967c5d16f62461bc8baed5253007145d22b4622
DimaSv <dimasvjet@gmail.com>
2023-03-14 21:30:31 +0200

    StringTotalEnd

--- src/main/java/com/example/home_work_4/StringTotal.java
+++ src/main/java/com/example/home_work_4/StringTotal.java
@@ -272,6 +272,120 @@ public class StringTotal {
         } else System.out.println(strPlus);
 
 
+        //String3_1
+        Scanner scanner = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String str1 = scanner.nextLine();
+        System.out.println("Enter the except of the word: ");
+        String exceptChar1 = sc1.nextLine();
+        System.out.println("Enter the except of the word: ");
+        String exceptChar2 = sc2.nextLine();
+        int count1 = acountLastChar(str1,exceptChar1);
+        int count2 = acountLastChar(str1,exceptChar2);
+        System.out.println(exceptChar1 + ": " + count1);
+        System.out.println(exceptChar2 + ": " + count2);
+        System.out.println("Total: "+(count1+count2));
+
+    }
+    static int acountLastChar(String str1, String exceptChar) {
+        int indexChar = 0;
+        int count = 0;
+        if (str1.contains(exceptChar)) {
+            while (indexChar >= 0) {
+                indexChar = str1.indexOf(exceptChar, indexChar);
+
+                if (indexChar < str1.length()-1){
+                    if (!Character.isLetter(str1.charAt(indexChar + 1))) {
+
+                        ++count;
+                        indexChar = str1.indexOf(exceptChar, indexChar+1);
+                        continue;
+                    } else {
+                        indexChar = str1.indexOf(exceptChar, indexChar+1);
+                        continue;}}
+                if (indexChar == str1.length()-1) {
+                    count++;
+                    break;
+                } break;
+
+            } return count;
+        } return 0;
+
+
+        //String3_2
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String str1 = scanner.nextLine();
+        int count1 = acountLastChar(str1);
+        System.out.println("Total: "+(count1));
+    }
+
+    static int acountLastChar(String str1) {
+        int indexChar = 2;
+        int count = 0;
+        while (str1.length() - indexChar > 0) {
+            if (str1.charAt(indexChar-1) == str1.charAt(indexChar) &&
+                    str1.charAt(indexChar-2) == str1.charAt(indexChar)) {
+                count++;
+            }
+            indexChar++;
+        } return count;
+
+        //String3_3
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your string: ");
+        String str1 = scanner.nextLine();
+        int count1 = acountLastChar(str1);
+        System.out.println("Total: "+(count1));
+    }
+
+    static int acountLastChar(String str1) {
+        int indexChar = 0;
+        int count = 0;
+        StringBuilder digital = new StringBuilder();
+        while (str1.length() - indexChar > 0) {
+            if (Character.isDigit(str1.charAt(indexChar))) {
+                digital.append(str1.charAt(indexChar));
+            }
+            digital.append(" ");
+            indexChar++;
+        }
+        digital = new StringBuilder(digital.toString().trim());
+        indexChar = 0;
+        int indexStart = 0;
+
+
+        if (digital.toString().contains("  ")) {
+            while (indexChar >= 0) {
+                indexChar = digital.indexOf("  ", indexStart);
+                count += Integer.parseInt(digital.substring(indexStart, indexChar).replaceAll(" ", ""));
+                System.out.println(count);
+                if (digital.charAt(indexChar+1) == ' ') {
+                    while (digital.charAt(indexChar) == ' ') {
+                        indexChar++;
+                    }
+                }
+                indexStart = indexChar;
+                indexChar = digital.indexOf("  ", indexStart);
+                if (indexChar < 0) {
+                    count += Integer.parseInt(digital.substring(indexStart, digital.length())
+                            .replaceAll(" ", ""));
+                    break;
+                }
+            }
+
+
+        } else {
+            count = Integer.parseInt(digital.toString().replaceAll(" ", ""));
+
+        }
+        return count;
+
+
+
+
 
 
     }
